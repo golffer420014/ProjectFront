2ec65a062fd96929e67f535bc036820bb02d5498
golffer420014 <142491843+golffer420014@users.noreply.github.com>
2023-10-13 05:17:58 +0700

    upgrade product card

--- Travel/screen/products/ProductCard.js
+++ Travel/screen/products/ProductCard.js
@@ -4,51 +4,56 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../Redux/Actions/cartActions';
 
+//icon
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import Entypo from 'react-native-vector-icons/Entypo'
+
 
 //Dimensions = เข้าถึงขนาดหน้าจอ
 var { width } = Dimensions.get('window')
 
 const ProductCard = (props) => {
-    const { name, price, image, countInStock } = props
+    const { name, rating , description } = props
 
     //  console.log('this is', JSON.stringify(props, null, 2))
 
 
 
     return (
-        <ScrollView>
         <View style={styles.container}>
         
-            <Image
-                style={styles.image}
-                resizeMode="contain"
-                // source={require('../../assests/1223348.jpg')}
-                source={{
-                    uri: image ?
-                        image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
-                }}
-            /> 
-            <View style={styles.card} />
-            <Text style={styles.title}>
-                {name.length > 15 ? name.substring(0, 15 - 3)
-                    + '...' : name
-                }
-            </Text>
-            <Text style={styles.price}>${price}</Text>
-
-            {countInStock > 0 ?(
-                    <View style={{marginBottom:10}}>
-                        <Button
-                            title={'Add'}
-                            onPress={() =>{
-                                props.addItemToCart(props)
-                            }}
-                         />
+                <View style={styles.imageContainer}>
+                    <Image
+                    style={[styles.image, {  width: 200, height: '100%' }]}
+                        source={require('../../assests/1223348.jpg')}
+                        // source={{
+                        //     uri: image ?
+                        //         image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                        // }}
+                    /> 
+            </View>
+            <View style={styles.details}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <Text style={styles.title} numberOfLines={1}>
+                        {name}
+                    </Text>
+                    <View style={{ flexDirection: 'row' , alignItems:'center' }}>
+                        <Text style={styles.rating} numberOfLines={1}>
+                            {rating} {''}
+                        </Text>
+                        <AntDesign name='star' color='#f36d72' size={15}/>
                     </View>
-                ) : <Text>หมด</Text>}
+                </View>
+                <View style={{ flexDirection: 'row' , alignItems:'center'}}>
+                    <Entypo name='location-pin' color='#f36d72' size={18} />
+                    <Text style={styles.location} numberOfLines={1}>
+                        {description}
+                    </Text>
+                </View>
+                
+            </View>
             
         </View>
-        </ScrollView>
     )
 }
 
@@ -64,39 +69,43 @@ export default connect(null, mapDispatchToProps)(ProductCard)
 
 const styles = StyleSheet.create({
     container: {
-        width: width / 2 - 20,
-        height: width / 1.7,
-        padding: 10,
-        borderRadius: 10,
-        marginTop: 50,
-        marginBottom: 5,
-        marginLeft: 10,
-        alignItems: 'center',
-        elevation: 8,
-        backgroundColor: 'white'
+        width: 182,
+        height: 200,
+        borderRadius: 20,
+        elevation: 1,
+        backgroundColor: 'white',
+        marginLeft:10,
+        marginTop:20,
+    },
+    imageContainer:{
+        flex: 1,
+        width:180,
+        marginLeft:11,
+        borderRadius:30,
+        borderTopRightRadius:30,
+        marginTop:10,
+        overflow: "hidden" ,
+
     },
     image: {
-        width: width / 2 - 20 - 10,
-        height: width / 2 - 20 - 40,
-        backgroundColor: 'transparent',
-        position: 'absolute',
-        top: -22,
+        aspectRatio:1.3,
+        resizeMode:'cover'
     },
-    card: {
-        marginBottom: 10,
-        height: width / 2 - 20 - 90,
-        backgroundColor: 'transparent',
-        width: width / 2 - 20 - 10
+    details:{
+        paddingHorizontal:20,
+        paddingVertical:10
     },
-    title: {
-        fontWeight: "bold",
-        fontSize: 14,
-        textAlign: 'center'
+    title:{
+        fontWeight:'bold',
+        color:'black',
+        fontSize:20,
+        marginBottom:2
     },
-    price: {
-        fontSize: 20,
-        color: 'orange',
-        marginTop: 10
+    location:{
+        fontSize:15
+    },
+    rating:{
+        fontSize:15
     }
 })
 
--- Travel/screen/products/ProductList.js
+++ Travel/screen/products/ProductList.js
@@ -9,13 +9,13 @@ const ProductList = (props) => {
     const { item } = props;
     return (
         <TouchableOpacity
-            style={{ width: '50%' , height:'auto'}}
+            style={{ width: '50%' }}
             onPress={() =>
                 props.navigation.navigate("Product Detail", { item: item })
             }
         >
             <View style={{
-                width: width / 1, 
+                width: width / 2, 
                 backgroundColor: 'gainsboro',
                 position:'relative'
             }}
