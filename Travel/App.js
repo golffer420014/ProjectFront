5d514f13ae0970778d440aba0bc727ccec859f9f
golffer420014 <142491843+golffer420014@users.noreply.github.com>
2024-01-28 14:36:25 +0700

    ช

--- Travel/App.js
+++ Travel/App.js
@@ -1,108 +1,125 @@
-import { StyleSheet, Text, View, LogBox } from 'react-native'
-import React , {useEffect} from 'react'
-import { NavigationContainer } from '@react-navigation/native';
+// import { StyleSheet, Text, View, LogBox } from 'react-native'
+// import React , {useEffect} from 'react'
+// import { NavigationContainer } from '@react-navigation/native';
 
 
 
-//noti
-import Toast from 'react-native-toast-message'
+// //noti
+// import Toast from 'react-native-toast-message'
 
-// redux
-import { Provider } from 'react-redux';
-import store from './Redux/Store';
+// // redux
+// import { Provider } from 'react-redux';
+// import store from './Redux/Store';
 
-// Context API
-import Auth from './context/store/Auth';
+// // Context API
+// import Auth from './context/store/Auth';
 
-// Navigator
-import Main from './Navigators/Main';
+// // Navigator
+// import Main from './Navigators/Main';
 
-//screen
-import { StatusBar } from '@gluestack-ui/themed'
-import UserEditPassowrd from './screen/user/UserEditPassowrd';
-import TestApi from './TestApi';
+// //screen
+// import { StatusBar } from '@gluestack-ui/themed'
+// import UserEditPassowrd from './screen/user/UserEditPassowrd';
+// import TestApi from './TestApi';
 
-// ไม่สนการแจ้งเตือน
-LogBox.ignoreAllLogs(true)
+// // ไม่สนการแจ้งเตือน
+// LogBox.ignoreAllLogs(true)
 
-  const toastConfig = {
-    success: ({text1, text2, props, ...rest}) => (
-      <View style={styles.successToas}>
-        <Text style={{color: 'black'}}>{text1}</Text>
-        <Text style={{color: 'gray'}}>{text2}</Text>
-      </View>
-    ),
-    error: ({text1, text2, props, ...rest}) => (
-      <View style={styles.errorToas}>
-        <Text style={{color: 'black'}}>{text1}</Text>
-        <Text style={{color: 'gray'}}>{text2}</Text>
-      </View>
-    ),
-    // ... other types if needed
-  };
+//   const toastConfig = {
+//     success: ({text1, text2, props, ...rest}) => (
+//       <View style={styles.successToas}>
+//         <Text style={{color: 'black'}}>{text1}</Text>
+//         <Text style={{color: 'gray'}}>{text2}</Text>
+//       </View>
+//     ),
+//     error: ({text1, text2, props, ...rest}) => (
+//       <View style={styles.errorToas}>
+//         <Text style={{color: 'black'}}>{text1}</Text>
+//         <Text style={{color: 'gray'}}>{text2}</Text>
+//       </View>
+//     ),
+//     // ... other types if needed
+//   };
 
-const App = () => {
+// const App = () => {
 
 
   
+//   return (
+//     <Auth >
+//       <Provider store={store}>
+
+//         <NavigationContainer >
+//           {/* <StatusBar
+//             hidden
+//           /> */}
+//           {/* <Header /> */}
+//           <Main  />
+//           <Toast ref={(ref) => Toast.setRef(ref)} config={toastConfig} />
+
+//         </NavigationContainer>
+//       </Provider>
+//     </Auth>
+
+
+//   );
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({
+//   center: {
+//     flex: 1,
+//     width: '100%',
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   errorToas: {
+//     borderWidth: 1,
+//     width: '80%',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'white',
+//     height: 55,
+//     borderRadius: 10,
+//     borderTopColor: '#dfdfdf',
+//     borderBottomColor: '#dfdfdf',
+//     borderLeftColor: 'red',
+//     borderLeftWidth: 5,
+//     borderRightColor: 'red',
+//     borderRightWidth: 5,
+//   },
+//   successToas: {
+//     borderWidth: 1,
+//     width: '80%',
+//     alignItems: 'center', // Center horizontally
+//     justifyContent: 'center', // Center vertically
+//     backgroundColor: 'white',
+//     height: 55,
+//     borderRadius: 10,
+//     borderTopColor: '#dfdfdf',
+//     borderBottomColor: '#dfdfdf',
+//     borderLeftColor: 'green',
+//     borderLeftWidth: 5,
+//     borderRightColor: 'green',
+//     borderRightWidth: 5,
+//   },
+// });
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TestApi from './TestApi'
+
+const App = () => {
   return (
-    <Auth >
-      <Provider store={store}>
-
-        <NavigationContainer >
-          {/* <StatusBar
-            hidden
-          /> */}
-          {/* <Header /> */}
-          <Main  />
-          <Toast ref={(ref) => Toast.setRef(ref)} config={toastConfig} />
-
-        </NavigationContainer>
-      </Provider>
-    </Auth>
-
-
-  );
+    <View>
+      <TestApi/>
+    </View>
+  )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  errorToas: {
-    borderWidth: 1,
-    width: '80%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    height: 55,
-    borderRadius: 10,
-    borderTopColor: '#dfdfdf',
-    borderBottomColor: '#dfdfdf',
-    borderLeftColor: 'red',
-    borderLeftWidth: 5,
-    borderRightColor: 'red',
-    borderRightWidth: 5,
-  },
-  successToas: {
-    borderWidth: 1,
-    width: '80%',
-    alignItems: 'center', // Center horizontally
-    justifyContent: 'center', // Center vertically
-    backgroundColor: 'white',
-    height: 55,
-    borderRadius: 10,
-    borderTopColor: '#dfdfdf',
-    borderBottomColor: '#dfdfdf',
-    borderLeftColor: 'green',
-    borderLeftWidth: 5,
-    borderRightColor: 'green',
-    borderRightWidth: 5,
-  },
-});
+const styles = StyleSheet.create({})
--- Travel/TestApi.js
+++ Travel/TestApi.js
@@ -1,127 +1,14 @@
-// In App.js in a new project
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
-import firebase from '@react-native-firebase/app';
-import auth from '@react-native-firebase/auth';
-// import auth from '@react-native-firebase/auth';
-
-GoogleSignin.configure({
-  webClientId:
-    '316031732426-mf4bc2laeqc2gokvclja906huuu8ksq7.apps.googleusercontent.com',
-});
-
-
-// Your Firebase config object
-const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-};
-
-// Check if Firebase has been initialized before initializing it
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-
-function Auth({navigation}) {
-  const [isSigninInProgress, setSigninInProgress] = React.useState(false);
-
-  async function onGoogleButtonPress() {
-    try {
-      // Check if your device supports Google Play
-      await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-      // Get the users ID token
-      const {idToken} = await GoogleSignin.signIn();
-      console.log({idToken});
-
-      // Create a Google credential with the token
-      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-      // Sign-in the user with the credential
-      return auth().signInWithCredential(googleCredential);
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        // operation (e.g. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // play services not available or outdated
-      } else {
-        // some other error happened
-      }
-    }
-  }
-
+const TestApi = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>auth Screen</Text>
-      <TouchableOpacity
-        onPress={() =>
-          onGoogleButtonPress().then(() => {
-            console.log('userLogin');
-            navigation.navigate('Home');
-          })
-        }
-        // disabled={isSigninInProgress}
-      >
-        <GoogleSigninButton
-          size={GoogleSigninButton.Size.Wide}
-          color={GoogleSigninButton.Color.Dark}
-          // disabled={isSigninInProgress}
-        />
-      </TouchableOpacity>
+    <View>
+      <Text>TestApi</Text>
     </View>
-  );
+  )
 }
 
-function HomeScreen({navigation}) {
-  const [email, setEmail] = useState();
+export default TestApi
 
-  // useEffect(() =>{
-  //   const decode = auth().onAuthStateChanged(user =>{
-  //     if(user){
-  //       setEmail(user.email)
-  //       console.log('user')
-  //     }else{
-  //       console.log('never login')
-  //       navigation.navigate('Home')
-  //     }
-  //   })
-
-  //   return() => decode();
-
-  // },[])
-
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home dd</Text>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function TestApi() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Auth" component={Auth} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default TestApi;
+const styles = StyleSheet.create({})
